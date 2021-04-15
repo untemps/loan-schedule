05d0f68fadec130f7706e1ccc48f593ea85fdfc2
untemps <6Mv2^2S6$2>
2021-04-15 20:56:06 +0200

    feat: Export LoanScheduleYears type

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { default as LoanSchedule, ILoanSchedule } from './LoanSchedule';
+export { default as LoanScheduleYears } from './LoanScheduleYears';
 export { default as LoanScheduleAmortization } from './LoanScheduleAmortization';
 export { default as LoanScheduleAmortizationType } from './LoanScheduleAmortizationType';
 export { default as LoanScheduleCompute } from './LoanScheduleCompute';
